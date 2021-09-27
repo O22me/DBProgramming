8fe4d5eb538fedbdb63e9cb194a9c4b3db26fbf4
김세훈 <sehoon97@naver.com>
2021-09-28 00:43:34 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBProgramming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click1(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text = "Hello World";
+        }
+    }
+}
